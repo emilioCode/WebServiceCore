ebaa477af838d758630bb6decab00223059819c1
Manuel E Mendez <emilio_mem@hotmail.com>
2021-03-09 00:42:14 -0400

    Add project files.

---
+++ WebServiceCore/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ServiceReference1;
+
+namespace WebServiceCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            consumeWebServiceHello("Miguel & Michael");
+        }
+
+        private static void consumeWebServiceHello(string name)
+        {
+            HelloEndpointClient client = new HelloEndpointClient();
+            helloRequest request = new helloRequest { Name = name };
+            SayHelloBody body = new SayHelloBody(request);
+            SayHello sayHello = new SayHello { Body = body };
+            SayHelloResponse response = client.SayHello(sayHello);
+
+            Console.WriteLine(response.Body.HelloResponse.Message);
+        }
+    }
+}
